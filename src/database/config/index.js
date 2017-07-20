c80236a9946401da48079c5b2504f9281f5b2b41
normchou <normchou@users.noreply.github.com>
2017-07-20 13:45:03 -0700

    remove yarn lock

--- src/database/config/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-	apiKey: "AIzaSyDCHl4_i1rzbgTBe_NHbp8CMijETJ8TW-A",
-	authDomain: "react-flashcard.firebaseapp.com",
-	databaseURL: "https://react-flashcard.firebaseio.com",
-	projectId: "react-flashcard",
-	storageBucket: "react-flashcard.appspot.com",
-	messagingSenderId: "101448422357"
-}
