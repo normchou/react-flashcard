2b4bc4bd3f8c82d02a117271c5a0c9449e9c46b1
normchou <normchou@users.noreply.github.com>
2017-07-21 08:46:09 -0700

    add vendor prefixes to support Safari

--- src/components/App/cardList.js
+++ src/components/App/cardList.js
@@ -10,19 +10,22 @@ const container = {
 }
 
 let flipper = {
+  WebkitTransition: '0.6s',
   transition: '0.6s',
+  WebKitTransformStyle: 'preserve-3d',
   transformStyle: 'preserve-3d',
   position: 'relative'
 }
 
 const flipContainer = {
+  WebkitPerspective: 4000,
   perspective: 4000,
   width: '90%',
   height: 400
 }
 
 const sharedCardContainer = {
-  width: '90%',
+  width: '100%',
   height: 400,
   boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
   backgroundColor: '#F7FFF7',
@@ -31,18 +34,19 @@ const sharedCardContainer = {
   justifyContent: 'center',
   alignItems: 'center',
   padding: 20,
-  width: '100%',
+  WebkitBackfaceVisibility: 'hidden',
   backfaceVisibility: 'hidden',
   position: 'absolute',
   top: 0,
   left: 0
 }
 
-let frontCardContainer = assign({}, sharedCardContainer, {
-  zIndex: 2
+const frontCardContainer = assign({}, sharedCardContainer, {
+  zIndex: 3
 })
 
-let backCardContainer = assign({}, sharedCardContainer, {
+const backCardContainer = assign({}, sharedCardContainer, {
+  WebkitTransform: 'rotateY(180deg)',
   transform: 'rotateY(180deg)'
 })
 
@@ -50,8 +54,8 @@ export default function CardList(props) {
   const { currentCard, totalCards, cards, showQuestion, toggleQuestion, onRemoveCard } = props;
   const { question, answer } = cards;
   flipper = showQuestion ? 
-    assign({}, flipper, { transform: 'rotateY(0deg)' }) :
-    assign({}, flipper, { transform: 'rotateY(180deg)' });
+    assign({}, flipper, { transform: 'rotateY(0deg)', WebkitTransform: 'rotateY(0deg)' }) :
+    assign({}, flipper, { transform: 'rotateY(180deg)', WebkitTransform: 'rotateY(0deg)' });
 
   return (
     <div style={container}>
